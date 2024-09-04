9ba47bc6c36babcdf6634630e6bce9dc02bf254a
Waine Oliveira Jr <waine@aerosim.io>
2024-09-04 20:29:49 -0300

    Minor updates

--- zigpy/proto_zig.py
+++ zigpy/proto_zig.py
@@ -1,9 +1,9 @@
 import zigpy.mzig.octree as zn
 
 print(dir(zn))
-domain_size = [2, 4]
-periodic_dim = [False, True]
+domain_size = (2, 4)
+periodic_dim = (False, True)
 
-a = zn.PyForest2D(tuple(domain_size), tuple(periodic_dim))
+a = zn.PyForest2D(domain_size, periodic_dim)
 
 print(a)
